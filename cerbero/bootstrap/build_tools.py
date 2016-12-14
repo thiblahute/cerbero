788335473d425b150dd67b7f422d56c228dce70b
Thibault Saunier <tsaunier@gnome.org>
2016-12-14 10:22:44 -0300

    Remove everything related to python3

--- cerbero/bootstrap/build_tools.py
+++ cerbero/bootstrap/build_tools.py
@@ -81,7 +81,6 @@ class BuildTools (BootstrapperBase):
         config.prefix = self.config.build_tools_prefix
         config.home_dir = self.config.home_dir
         config.load()
-        config.variants.python3 = False
 
         config.prefix = self.config.build_tools_prefix
         config.build_tools_prefix = self.config.build_tools_prefix
